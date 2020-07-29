6c07dd9f0641bcc75d4af694ae2f6cd726da11d2
Nickolazz1 <elanir358@gmail.com>
2020-07-29 17:44:35 +0300

    starter template for blog auth

---
+++ landing/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
+app = Flask(__name__)
+app.secret_key = 'somesecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+manager = LoginManager(app)
+
+
+from landing import models, routes
+
+
+admin = Admin(app)
+admin.add_view(ModelView(models.User, db.session))
+
+db.create_all()
