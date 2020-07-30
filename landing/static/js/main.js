b8add9569374014c657378aadff978b8d1971e10
Nickolazz1 <elanir358@gmail.com>
2020-07-30 18:01:24 +0300

    front-end materialize css, carousel

---
+++ landing/static/js/main.js
@@ -0,0 +1,6 @@
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+});
