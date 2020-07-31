296b926443be95212cea7f32840a1e2bd8e84f3a
Nickolazz1 <elanir358@gmail.com>
2020-07-31 12:48:48 +0300

    error options is not defined repaired

--- landing/static/js/main.js
+++ landing/static/js/main.js
@@ -1,6 +1,7 @@
 M.AutoInit();
-
+// error "options is not defined",delete options var argument from instances
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
+    var instances = M.Carousel.init(elems);
+    console.log("Event listener worked");
 });
