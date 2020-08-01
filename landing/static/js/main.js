80e6651cc7ae5e202cd1b11d87bcc2b0c755bef3
Nickolazz1 <elanir358@gmail.com>
2020-08-01 13:31:51 +0300

    material design, header refresh

--- landing/static/js/main.js
+++ landing/static/js/main.js
@@ -1,7 +1,21 @@
 M.AutoInit();
-// error "options is not defined",delete options var argument from instances
+
+// header navbar
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
+
+// slider
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+});
+
+// carousel
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems);
-    console.log("Event listener worked");
+    //console.log("Event listener worked");
+    //error "options is not defined",delete options var argument from instances
 });
