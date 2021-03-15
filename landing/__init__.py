08baa30bd644cc3dc5379d6b9802378924ec2527
salamanka <elanir358@gmail.com>
2021-03-15 14:46:43 +0200

    think about dash

--- landing/__init__.py
+++ landing/__init__.py
@@ -32,3 +32,47 @@ admin.add_view(models.MyModelView(models.Message, db.session))
 admin.add_view(models.MyModelView(models.Tag, db.session))
 
 #db.create_all()
+
+# DASH plotly tutorial
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+dashapp = dash.Dash(
+    __name__,
+    server=app,
+    url_base_pathname='/dash/',
+    external_stylesheets=external_stylesheets
+)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+dashapp.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+@app.route('/dash')
+def my_dash_app():
+    return dashapp.index()
